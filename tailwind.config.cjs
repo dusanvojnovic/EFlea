d93be56aebec0b7f1b5d93d3ab547fccac8e4e72
Your Name <you@example.com>
2022-10-25 13:50:36 +0200

    import google font and define global styles

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "color-blue": "var(--color-blue)",
+        "color-violet": "var(--color-violet)",
+        "color-violet-light": "var(--color-violet-light)",
+        "color-pinkish": "var(--color-pinkish)",
+      },
+      gridTemplateColumns: {
+        fluid: "repeat(auto-fit, minmax(300px, 1fr))",
+      },
+    },
   },
   plugins: [],
 };
