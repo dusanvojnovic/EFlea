4e8c365feedebbfeae6ca32e321a3518f19c2c22
Your Name <you@example.com>
2022-10-28 11:37:19 +0200

    change css classes

--- src/components/SearchBox/SearchBox.tsx
+++ src/components/SearchBox/SearchBox.tsx
@@ -13,18 +13,18 @@ export const SearchBox: React.FunctionComponent = () => {
 
   return (
     <form
-      className="mx-auto mt-[10rem] mb-0 flex w-[50rem] justify-between self-center rounded-md bg-gray p-6"
+      className="mx-auto mt-[10rem] mb-0 flex w-[85%] flex-col justify-between  rounded-md bg-gray p-6 md:w-[50rem] xs:flex-row"
       onSubmit={search}
     >
       <input
         type="text"
         ref={searchRef}
         placeholder="Search..."
-        className="h-[3rem] w-[35rem] rounded-md border border-green indent-4 text-[1.5rem]"
+        className="h-[3rem] w-full rounded-md border border-green indent-4 text-[1.5rem] focus:outline-none xs:w-[75%]"
       />
       <button
         type="submit"
-        className="w-[6.5rem] cursor-pointer rounded-md border border-green text-[1.3rem]"
+        className="mt-4 h-[3rem] w-[6.5rem] cursor-pointer self-center  rounded-md border border-green text-[1.3rem] xs:mt-0 "
       >
         Search
       </button>
