a0989bf9526f5d56cd1a60f591aabc0d65ecd51f
Your Name <you@example.com>
2022-11-01 11:49:30 +0100

    remove? modalOverlay component

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -12,26 +12,26 @@ export const Backdrop: React.FunctionComponent<BackdropProps> = ({
   return ReactDOM.createPortal(
     <div
       onClick={onClick}
-      className="fixed top-0 left-0 z-20 h-full w-full bg-black/90"
+      className="fixed inset-0 z-10 h-full w-full bg-black/95"
     />,
     document.getElementById("backdrop")!
   );
 };
 
-export interface ModalOverlayProps {
-  children: React.ReactNode;
-}
+// export interface ModalOverlayProps {
+//   children: React.ReactNode;
+// }
 
-export const ModalOverlay: React.FunctionComponent<ModalOverlayProps> = ({
-  children,
-}) => {
-  return ReactDOM.createPortal(
-    <div className="fixed top-[50%] left-[50%] z-50 w-[60vw] -translate-y-1/2 -translate-x-1/2 rounded-xl bg-yellow-200">
-      {children}
-    </div>,
-    document.getElementById("overlays")!
-  );
-};
+// export const ModalOverlay: React.FunctionComponent<ModalOverlayProps> = ({
+//   children,
+// }) => {
+//   return ReactDOM.createPortal(
+//     <div className="flex min-h-[100vh] items-center justify-center bg-yellow-200">
+//       {children}
+//     </div>,
+//     document.getElementById("overlays")!
+//   );
+// };
 
 export interface ModalProps {
   children: React.ReactNode;
@@ -51,7 +51,7 @@ export const Modal: React.FunctionComponent<ModalProps> = ({
   return isBrowser ? (
     <>
       <Backdrop onClick={onClick} />
-      <ModalOverlay>{children}</ModalOverlay>
+      {children}
     </>
   ) : null;
 };
