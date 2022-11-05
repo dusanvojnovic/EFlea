19c72200b73dbe6afb5c0518a1eeb1340fcdc3ee
Your Name <you@example.com>
2022-11-05 14:25:59 +0100

    remove formik, add react-hook-form

--- src/components/Forms/Input/Input.tsx
+++ src/components/Forms/Input/Input.tsx
@@ -1,64 +1,77 @@
 import React, { useState } from "react";
 import { BsEye, BsEyeSlash } from "react-icons/bs";
 import { IconContext } from "react-icons";
+import {
+  UseFormRegister,
+  FieldValues,
+  ValidationRule,
+  Message,
+  FieldErrorsImpl,
+} from "react-hook-form";
 
 interface InputProps {
-  id: string;
   element?: string;
   placeholder: string;
   name: string;
   type?: string;
   isPasswordField?: boolean;
-  onChange: React.ChangeEventHandler<HTMLInputElement>;
+  register: UseFormRegister<FieldValues>;
+  validationSchema?: {
+    validate?: { passwordEqual: (val: string) => boolean | string };
+    required?: Message | ValidationRule<boolean>;
+    minLength?: ValidationRule<number>;
+    pattern?: ValidationRule<RegExp>;
+  };
+  errors: Partial<FieldErrorsImpl<{ [x: string]: string }>>;
+  isDirty?: boolean;
 }
 
 export const Input = React.forwardRef<HTMLInputElement, InputProps>(
   (
     {
-      id,
       element = "input",
       placeholder,
       type = "text",
       isPasswordField = false,
       name,
-      onChange,
+      register,
+      validationSchema,
+      isDirty = false,
+      errors,
       ...props
     },
     ref
   ) => {
     const [passwordIsVisible, setPasswordIsVisible] = useState<boolean>(false);
-    const [userIsTyping, setUserIsTyping] = useState<boolean>(false);
 
     return (
-      <div className="flex p-4 text-[1.5rem]">
+      <div className="flex flex-col p-4 text-[1.5rem]">
         {element === "input" ? (
           <input
-            onChange={() => {
-              onChange;
-              setUserIsTyping(true);
-            }}
-            className="w-full rounded-md border border-solid border-green p-4"
+            className="relative w-full rounded-md border border-solid border-green p-4"
             type={isPasswordField && !passwordIsVisible ? "password" : "text"}
             placeholder={placeholder}
-            id={id}
-            name={name}
+            id={name}
+            {...register(name, validationSchema)}
             {...props}
           />
         ) : (
           <textarea
             className="w-full border border-solid border-green p-4"
-            id={id}
-            name={id}
+            name={name}
             placeholder={placeholder}
             rows={8}
           />
         )}
+        {errors && (
+          <p className="pt-2 text-[1.4rem] text-red">{errors[name]?.message}</p>
+        )}
 
-        {isPasswordField && userIsTyping ? (
+        {isPasswordField && isDirty ? (
           <IconContext.Provider value={{ size: "1.5rem", color: "black" }}>
             <i
               onClick={() => setPasswordIsVisible(!passwordIsVisible)}
-              className="mt-4 ml-[-2.5rem] cursor-pointer"
+              className="absolute mt-6 ml-[31rem] cursor-pointer"
             >
               {!passwordIsVisible ? <BsEyeSlash /> : <BsEye />}
             </i>
--- src/components/Forms/RegisterForm/RegisterForm.tsx
+++ src/components/Forms/RegisterForm/RegisterForm.tsx
@@ -1,140 +1,125 @@
 import Link from "next/link";
 import React from "react";
 import { Input } from "../Input/Input";
-import { Formik, Form } from "formik";
-import * as Yup from "yup";
-
-const initialValues = {
-  firstName: "",
-  lastName: "",
-  email: "",
-  city: "",
-  password: "",
-  passwordConfirmed: "",
-};
-
-const validationSchema = Yup.object().shape({
-  firstName: Yup.string()
-    .min(2, "First name must be at least 2 characters long")
-    .required("First name is required"),
-  lastName: Yup.string()
-    .min(2, "Last name must be at least 2 characters long")
-    .required("Last name is required"),
-  email: Yup.string()
-    .email("Please provide valid email address")
-    .required("Email is required"),
-  city: Yup.string()
-    .min(2, "City name must be at least 2 characters long")
-    .required("City name is required"),
-  password: Yup.string()
-    .min(6, "Password must be at least 6 characters long")
-    .required("Password is required"),
-  passwordConfirmed: Yup.string()
-    .oneOf(
-      [Yup.ref("password"), null],
-      "Passwords doesn't match, please try again"
-    )
-    .required("Password confirmation is required"),
-});
+import { FieldValues, useForm } from "react-hook-form";
 
 export const RegisterForm: React.FunctionComponent = () => {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    getValues,
+    formState: { errors, dirtyFields },
+  } = useForm<FieldValues>({
+    reValidateMode: "onSubmit",
+    defaultValues: {
+      password: "",
+      passwordConfirmed: "",
+    },
+  });
+
+  const onSubmit = (data: any) => {
+    console.log(data);
+    reset();
+  };
+
   return (
     <div className="bg-gray-200 mx-auto my-[15rem] flex w-[40rem] flex-col justify-center rounded-md border-[2px] border-solid border-green p-8 ">
-      <Formik
-        initialValues={initialValues}
-        validationSchema={validationSchema}
-        onSubmit={(values, { setErrors }) => {
-          console.log(values);
-        }}
-      >
-        {({ isSubmitting, isValid, dirty, handleChange, errors, touched }) => (
-          <Form>
-            <Input
-              id="firstName"
-              placeholder="First Name"
-              name="firstName"
-              onChange={handleChange}
-            />
-            {touched.firstName && errors.firstName ? (
-              <div className="text-red-400 pl-4 text-[1.4rem]">
-                {errors.firstName}
-              </div>
-            ) : null}
-            <Input
-              id="lastName"
-              placeholder="Last Name"
-              name="lastName"
-              onChange={handleChange}
-            />
-            {touched.lastName && errors.lastName ? (
-              <div className="text-red-400 pl-4 text-[1.4rem]">
-                {errors.lastName}
-              </div>
-            ) : null}
-            <Input
-              id="email"
-              placeholder="Email"
-              name="email"
-              type="email"
-              onChange={handleChange}
-            />
-            {touched.email && errors.email ? (
-              <div className="text-red-400 pl-4 text-[1.4rem]">
-                {errors.email}
-              </div>
-            ) : null}
-            <Input
-              id="city"
-              placeholder="City"
-              name="city"
-              onChange={handleChange}
-            />
-            {touched.city && errors.city ? (
-              <div className="text-red-400 pl-4 text-[1.4rem]">
-                {errors.city}
-              </div>
-            ) : null}
-            <Input
-              onChange={handleChange}
-              id="password"
-              placeholder="Password"
-              name="password"
-              type="password"
-              isPasswordField
-            />
-            {touched.password && errors.password ? (
-              <div className="text-red-400 pl-4 text-[1.4rem]">
-                {errors.password}
-              </div>
-            ) : null}
-            <Input
-              onChange={handleChange}
-              id="passwordConfirmed"
-              placeholder="Password Confirmed"
-              name="passwordConfirmed"
-              type="password"
-              isPasswordField
-            />
-            {touched.passwordConfirmed && errors.passwordConfirmed ? (
-              <div className="text-red-400 pl-4 text-[1.4rem]">
-                {errors.passwordConfirmed}
-              </div>
-            ) : null}
-            <button
-              className="mx-auto my-8 flex w-[80%] cursor-pointer justify-center self-center rounded-md border-none bg-red p-4 text-[1.5rem] text-white hover:bg-red disabled:cursor-not-allowed"
-              type="submit"
-              disabled={!isValid}
-            >
-              Register
-            </button>
-          </Form>
-        )}
-      </Formik>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Input
+          name="firstName"
+          placeholder="First Name"
+          register={register}
+          validationSchema={{
+            required: "First Name is required",
+            minLength: {
+              value: 2,
+              message: "First name must be at least 2 characters long",
+            },
+          }}
+          errors={errors}
+        />
+        <Input
+          name="lastName"
+          placeholder="Last Name"
+          register={register}
+          validationSchema={{
+            required: "Last Name is required",
+            minLength: {
+              value: 2,
+              message: "Last name must be at least 2 characters long",
+            },
+          }}
+          errors={errors}
+        />
+        <Input
+          name="email"
+          placeholder="Email"
+          register={register}
+          validationSchema={{
+            required: "Email is required",
+            pattern: {
+              message: "Enter valid email address",
+              value:
+                /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/,
+            },
+          }}
+          errors={errors}
+        />
+        <Input
+          name="city"
+          placeholder="City"
+          register={register}
+          validationSchema={{
+            required: "City is required",
+            minLength: {
+              value: 2,
+              message: "City name must be at least 2 characters long",
+            },
+          }}
+          errors={errors}
+        />
+        <Input
+          name="password"
+          isPasswordField
+          isDirty={dirtyFields.password}
+          placeholder="Password"
+          register={register}
+          validationSchema={{
+            required: "Password is required",
+            minLength: {
+              value: 8,
+              message: "Password must be at least 8 characters long",
+            },
+          }}
+          errors={errors}
+        />
+        <Input
+          name="passwordConfirmed"
+          isPasswordField
+          isDirty={dirtyFields.passwordConfirmed}
+          placeholder="Confirm Password"
+          register={register}
+          validationSchema={{
+            validate: {
+              passwordEqual: (value: string) =>
+                value === getValues("password") || "Password not match",
+            },
+          }}
+          errors={errors}
+        />
+        <button
+          className="mx-auto my-8 flex w-[80%] cursor-pointer justify-center self-center rounded-md border-none bg-red p-4 text-[1.5rem] text-white hover:bg-red disabled:cursor-not-allowed"
+          type="submit"
+        >
+          Login
+        </button>
+      </form>
       <div className="self-center text-[1.5rem]">
         <p>
           Already have account? Login &nbsp;
           <Link href="/login">
-            <a className="text-green">here</a>
+            <a className="text-green">here.</a>
           </Link>
         </p>
       </div>
