b02c11ba643e8edc10a6799652f3ba58c21297b9
Your Name <you@example.com>
2022-11-25 23:33:19 +0100

    add credentials provider for login

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,30 +1,70 @@
 import NextAuth, { type NextAuthOptions } from "next-auth";
-import DiscordProvider from "next-auth/providers/discord";
-
 // Prisma adapter for NextAuth, optional and can be removed
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { prisma } from "../../../server/db/client";
-import { env } from "../../../env/server.mjs";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { loginUserSchema } from "../../../schema/user.schema";
+import { compare } from "bcrypt";
+import { User } from "@prisma/client";
 
 export const authOptions: NextAuthOptions = {
-  // Include user.id on session
+  session: {
+    strategy: "jwt",
+  },
+  providers: [
+    CredentialsProvider({
+      name: "credentials",
+      credentials: {
+        email: { type: "email" },
+        password: { type: "password" },
+      },
+      authorize: async (credentials) => {
+        const creds = await loginUserSchema.parseAsync(credentials);
+        const user = await prisma.user.findFirst({
+          where: {
+            email: creds.email,
+          },
+        });
+
+        if (!user) {
+          return null;
+        }
+
+        const isValidPassword = await compare(creds.password, user.password);
+        if (!isValidPassword) {
+          return null;
+        }
+
+        console.log("logged in");
+
+        return {
+          id: user.id,
+          email: user.email,
+        };
+      },
+    }),
+  ],
   callbacks: {
-    session({ session, user }) {
-      if (session.user) {
-        session.user.id = user.id;
+    async jwt({ token, user }) {
+      if (user) {
+        // token = user;
+        token.user = user;
       }
-      return session;
+      return Promise.resolve(token);
     },
+    async session({ session, token }) {
+      if (session.user) {
+        session.user = token.user as User;
+      }
+      return Promise.resolve(session);
+    },
+  },
+  pages: {
+    signIn: "/login",
+    newUser: "/register",
   },
   // Configure one or more authentication providers
   adapter: PrismaAdapter(prisma),
-  providers: [
-    DiscordProvider({
-      clientId: env.DISCORD_CLIENT_ID,
-      clientSecret: env.DISCORD_CLIENT_SECRET,
-    }),
-    // ...add more providers here
-  ],
 };
 
 export default NextAuth(authOptions);
