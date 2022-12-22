c4c7348f214ffa947526597e3a1129e6d936484e
Your Name <you@example.com>
2022-12-22 16:09:32 +0100

    add item model in prisma, run migrations

---
+++ prisma/migrations/20221130112531_migration/migration.sql
@@ -0,0 +1,11 @@
+-- CreateIndex
+CREATE INDEX `Account_userId_idx` ON `Account`(`userId`);
+
+-- CreateIndex
+CREATE INDEX `Image_itemId_idx` ON `Image`(`itemId`);
+
+-- CreateIndex
+CREATE INDEX `Item_userId_idx` ON `Item`(`userId`);
+
+-- CreateIndex
+CREATE INDEX `Session_userId_idx` ON `Session`(`userId`);
