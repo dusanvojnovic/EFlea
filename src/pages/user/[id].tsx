061dc5b6ca1b63f9a650eb08c042b77385569817
Your Name <you@example.com>
2022-12-29 17:24:15 +0100

    pass the user's items as props to the items list

--- src/pages/user/[id].tsx
+++ src/pages/user/[id].tsx
@@ -1,9 +1,18 @@
+import { Item } from "@prisma/client";
+import { useSession } from "next-auth/react";
 import Link from "next/link";
 import React from "react";
 import { ItemsList } from "../../components/Items/ItemsList";
 import { Layout } from "../../components/Layout/Layout";
+import { trpc } from "../../utils/trpc";
 
 const UserPage: React.FunctionComponent = ({}) => {
+  const { data: session } = useSession();
+  if (!session?.user) return <h1>user not found</h1>;
+  const { data: userItems } = trpc.item.getItemsByUser.useQuery({
+    id: session.user.id,
+  });
+
   return (
     <Layout>
       <Link href="/new-item">
@@ -11,7 +20,7 @@ const UserPage: React.FunctionComponent = ({}) => {
           add new item
         </button>
       </Link>
-      <ItemsList category="" />
+      <ItemsList items={userItems as Item[]} />
     </Layout>
   );
 };
