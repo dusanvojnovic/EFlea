660865fd00ef6dcfc70efce07ad9b6644ecea6c5
Your Name <you@example.com>
2022-12-29 17:25:45 +0100

    pass the category items as props to the items list

--- src/pages/category/[category]/index.tsx
+++ src/pages/category/[category]/index.tsx
@@ -1,15 +1,19 @@
-import { useRouter } from "next/router";
+import { Item } from "@prisma/client";
 import React from "react";
 import { ItemsList } from "../../../components/Items/ItemsList";
 import { Sidebar } from "../../../components/Sidebar/Sidebar";
+import { trpc } from "../../../utils/trpc";
 
 interface CategoryPageProps {
   category: string;
 }
 
-const CategoryPage: React.FunctionComponent<CategoryPageProps> = ({}) => {
-  const router = useRouter();
-  const { category } = router.query;
+const CategoryPage: React.FunctionComponent<CategoryPageProps> = ({
+  category,
+}) => {
+  const { data: categoryItems } = trpc.item.getItemsByCategory.useQuery({
+    category: category,
+  });
 
   return (
     <div className="mx-auto flex justify-between">
@@ -17,7 +21,7 @@ const CategoryPage: React.FunctionComponent<CategoryPageProps> = ({}) => {
         <Sidebar />
       </div>
       <div className="flex flex-col">
-        <ItemsList category={category as string} />
+        <ItemsList category={category} items={categoryItems as Item[]} />
       </div>
     </div>
   );
