1a91df185e659c57220e74743f1111215ad16a67
dusanvojnovic <dusan_vojnovic@yahoo.com>
2023-01-11 15:26:02 +0100

    add new prop remove the unnecessary

--- src/components/Forms/Select/Select.tsx
+++ src/components/Forms/Select/Select.tsx
@@ -3,19 +3,20 @@ import { FieldValues, UseFormRegister, ValidationRule } from "react-hook-form";
 
 export interface SelectProps {
   name: string;
-  value?: string;
   options: string[];
+  categoryIndex?: number;
   register: UseFormRegister<FieldValues>;
   validationSchema?: {
     required?: string | ValidationRule<boolean>;
   };
-  onChange?: ChangeEventHandler<HTMLSelectElement> | undefined;
+  onChange: ChangeEventHandler<HTMLSelectElement>;
 }
 
 export const Select: React.FunctionComponent<SelectProps> = ({
   register,
   options,
   validationSchema,
+  categoryIndex,
   name,
   onChange,
 }) => {
@@ -25,8 +26,11 @@ export const Select: React.FunctionComponent<SelectProps> = ({
       onChange={onChange}
       className="relative w-full rounded-md border border-solid border-green p-4 text-[1.5rem]"
     >
-      {options.map((value) => (
-        <option key={value} value={value}>
+      <option disabled selected>
+        Select a category...
+      </option>
+      {options.map((value, index) => (
+        <option key={value} value={value} selected={index === categoryIndex}>
           {value}
         </option>
       ))}
