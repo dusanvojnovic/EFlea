198c3d34fee2b0b8829440dd1a29f65873d489e5
Your Name <you@example.com>
2023-02-13 18:46:43 +0100

    rename component

--- src/components/categories/CategoriesPreviewList.tsx
+++ src/components/categories/CategoriesPreviewList.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { CategoryPreviewItem } from "./CategoryPreviewItem";
+import { CategoryPreview } from "./CategoryPreview";
 
 interface CategoriesPreviewListProps {
   categories: string[];
@@ -12,7 +12,7 @@ export const CategoriesPreviewList: React.FunctionComponent<
     <div className="my-40 flex flex-col content-center justify-center">
       {/* <div className="mx-12 my-[12rem] grid w-[70%] grid-cols-fluid gap-[10rem] capitalize sm:mx-auto"> */}
       {categories.map((category) => {
-        return <CategoryPreviewItem key={category} categoryName={category} />;
+        return <CategoryPreview key={category} categoryName={category} />;
       })}
     </div>
   );
