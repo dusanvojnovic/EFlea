a4beddf4b152dbfb5398903dea3d149ef2120190
Your Name <you@example.com>
2023-02-21 15:34:30 +0100

    include subcategory to items procedures

--- src/schema/item.schema.ts
+++ src/schema/item.schema.ts
@@ -3,12 +3,13 @@ import z from "zod";
 export const itemSchema = z.object({
   title: z.string(),
   category: z.string(),
+  subcategory: z.string(),
   description: z.string(),
   price: z.string(),
   acceptExchange: z.boolean(),
   fixedPrice: z.boolean(),
   imgFiles: z.any().nullish(),
-  imagesUrl: z.array(z.string()),
+  imagesUrl: z.array(z.string()).nullish(),
 });
 
 export type ItemType = z.TypeOf<typeof itemSchema>;
