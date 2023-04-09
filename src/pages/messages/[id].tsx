f12db260d07ec1172aacd932b6484edc088ab9d5
Your Name <you@example.com>
2023-04-09 14:44:49 +0200

    and new event and chat page

--- src/pages/messages/[id].tsx
+++ src/pages/messages/[id].tsx
@@ -1,39 +1,26 @@
-import { NextPage } from "next";
-import { useState } from "react";
-import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
-import { Input } from "../../components/Forms/Input/Input";
-import { MessageType } from "../../schema/message.schema";
-import { trpc } from "../../utils/trpc";
+import { GetServerSideProps, NextPage } from "next";
+import { Session } from "next-auth";
+import { getSession, useSession } from "next-auth/react";
+import { Chat } from "../../components/Chat/Chat";
 
-const UserPage: NextPage = () => {
-  const { register, handleSubmit } = useForm<FieldValues>({
-    reValidateMode: "onSubmit",
-  });
-
-  const [message, setMessage] = useState("");
-  const [messages, setMessages] = useState<MessageType[]>([]);
-
-  const { mutateAsync: sendMessage } = trpc.room.sendMessage.useMutation({});
-
-  const onSubmit = async () => {
-    console.log("a");
-  };
+const ChatPage: NextPage = () => {
+  const { data: session } = useSession();
 
   return (
-    <div className="bg-gray-200 relative mx-auto my-[15rem] h-32 w-[70rem] justify-center rounded-md">
-      <form
-        className=""
-        onSubmit={handleSubmit(onSubmit as SubmitHandler<FieldValues>)}
-      >
-        <Input
-          element="textarea"
-          name="message"
-          register={register}
-          placeholder="Write a message..."
-        />
-      </form>
+    <div className="bg-gray-200 absolute inset-0 h-screen">
+      <Chat session={session as Session} />
     </div>
   );
 };
 
-export default UserPage;
+export default ChatPage;
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const session = await getSession(context);
+
+  return {
+    props: {
+      session,
+    },
+  };
+};
